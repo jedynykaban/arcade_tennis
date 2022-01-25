3deee4eaf5240af4f800c74ba1a55b13329f150a
kaban <michal.odrzywolek@doctrin.se>
2022-01-25 19:46:32 +0100

    Fix variables naming, unification to English language naming (#1)

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+#run > python -m pip install pygame (in python/scripts folder to install pygame module)
+
 import pygame, sys
 
-screen = pygame.display.set_mode((1280,720))
-gracz1 = pygame.Rect(50,50,50,150)
-gracz2 = pygame.Rect(1180,50,50,150)
+screen = pygame.display.set_mode((1280, 720))
+player1 = pygame.Rect(50, 50, 50, 150)
+player2 = pygame.Rect(1180, 50, 50, 150)
 clock = pygame.time.Clock()
-kolox = 610
-koloy = 330
-speedx = 1
-speedy = 1
+ballX = 610
+ballY = 330
+speedX = 1
+speedY = 1
 
 while True:
     for event in pygame.event.get():
@@ -15,42 +17,42 @@ while True:
             sys.exit(0)
 
     if pygame.key.get_pressed()[pygame.K_s]:
-        gracz1.y += 10
+        player1.y += 10
     if pygame.key.get_pressed()[pygame.K_w]:
-        gracz1.y -= 10
+        player1.y -= 10
 
     if pygame.key.get_pressed()[pygame.K_DOWN]:
-        gracz2.y += 10
+        player2.y += 10
     if pygame.key.get_pressed()[pygame.K_UP]:
-        gracz2.y -= 10
+        player2.y -= 10
 
-    kolox += speedx
-    koloy += speedy
+    ballX += speedX
+    ballY += speedY
 
-    if koloy >= 690:
-        speedy *= -1
+    if ballY >= 690:
+        speedY *= -1
 
-    if koloy <= 30:
-        speedy *= -1
+    if ballY <= 30:
+        speedY *= -1
 
-    if kolox >= 1250:
-        kolox, koloy = 610, 330
-        speedx *= -1
+    if ballX >= 1250:
+        ballX, ballY = 610, 330
+        speedX *= -1
 
-    if kolox <= 30:
-        kolox, koloy = 610, 330
-        speedx *= -1
+    if ballX <= 30:
+        ballX, ballY = 610, 330
+        speedX *= -1
 
-    if kolox > gracz1.y and kolox < gracz1.y + 50:
+    if ballX > player1.y and ballX < player1.y + 50:
         print("fsafsa")
-        #speedx *= -1
+        #speedX *= -1
 
     # print(kolox)
 
-    screen.fill((35,35,35))
-    pygame.draw.rect(screen, (50,50,200), gracz1)
-    pygame.draw.rect(screen, (50, 50, 200), gracz2)
-    pygame.draw.circle(screen, (50,200,50), (kolox, koloy), 30)
+    screen.fill((35, 35, 35))
+    pygame.draw.rect(screen, (50, 50, 200), player1)
+    pygame.draw.rect(screen, (50, 50, 200), player2)
+    pygame.draw.circle(screen, (50, 200, 50), (ballX, ballY), 30)
     pygame.display.flip()
 
-    clock.tick(60)
+    clock.tick(100)
